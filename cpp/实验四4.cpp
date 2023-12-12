7cc3833244728cacf5b3697f6794e5f6d7bf51cd
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-12-12 12:29:32 +0800

    Update 实验四4.cpp

--- cpp/实验四4.cpp
+++ cpp/实验四4.cpp
@@ -1,35 +1,51 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 void merge(const int list1[], int size1, const int list2[], int size2, int list3[]) {
-	for (int i = 0;i < size1;i++)
-		list3[i] = list1[i];
-	for (int i = size1;i < size1+size2;i++)
-		list3[i] = list1[i-size1];
-	bool changed = true;
-	do
-	{
-		changed = false;
-		for (int j = 0; j < 9; j++)
-			if (list3[j] > list3[j + 1])
-			{
-				swap(list3[j], list3[j + 1]);
-				changed = true;
-			}
-	} while (changed);
+    int i = 0; 
+    int j = 0;  
+    int k = 0;  
+    while (i < size1 && j < size2) {
+        if (list1[i] <= list2[j]) {
+            list3[k] = list1[i];
+            i++;
+        }
+        else {
+            list3[k] = list2[j];
+            j++;
+        }
+        k++;
+    }
+    while (i < size1) {
+        list3[k] = list1[i];
+        i++;
+        k++;
+    }
+    while (j < size2) {
+        list3[k] = list2[j];
+        j++;
+        k++;
+    }
 }
 int main() {
-	int list1[80] = {};
-	int list2[80] = {};
-	int list3[160] = {};
-	int a, b;
-	cout << "list1µÄÊý×Ö¸öÊý£º";
-	cin >> a;
-	for (int i = 0;i < a;i++)
-		cin >> list1[i];
-	cout << "list2µÄÊý×Ö¸öÊý£º";
-	cin >> b;
-	for (int i = 0;i < b;i++)
-		cin >> list2[i];
-	merge(list1, a, list2, b,list3);
-	return 0;
+    const int MAX_SIZE = 80;
+    int size1, size2;
+    int list1[MAX_SIZE], list2[MAX_SIZE], list3[MAX_SIZE * 2];
+
+    cout << "è¯·è¾“å…¥æ•°ç»„1çš„å¤§å°å’Œå…ƒç´ ï¼š" << endl;
+    cin >> size1;
+    for (int i = 0; i < size1; i++) {
+        cin >> list1[i];
+    }
+    cout << "è¯·è¾“å…¥æ•°ç»„2çš„å¤§å°å’Œå…ƒç´ ï¼š" << endl;
+    cin >> size2;
+    for (int i = 0; i < size2; i++) {
+        cin >> list2[i];
+    }
+    merge(list1, size1, list2, size2, list3);
+    cout << "åˆå¹¶åŽçš„æ•°ç»„ä¸ºï¼š" << endl;
+    for (int i = 0; i < size1 + size2; i++) {
+        cout << list3[i] << " ";
+    }
+    cout << endl;
+    return 0;
 }
