804d778a01a1fbf44151a4d0de63cb80e4af316a
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-12-12 12:18:06 +0800

    Update 实验四3.cpp

--- cpp/实验四3.cpp
+++ cpp/实验四3.cpp
@@ -1,14 +1,18 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
 int main() {
-	bool lijun[100] = {};
-	for (int i = 0;i <= 99;i++)
-		lijun[i] = 0;
-	for (int i=1;i <= 100;i++) {
-		for (int j = i; j <= 100;j += i)
-			lijun[j] = !lijun[j];
-	}
-	for (int i = 0;i <= 99;i++)
-		cout << lijun[i] << "\t";
-	return 0;
+    bool lijun[100] = {};
+    
+    for (int i = 1; i < 100; i++) {
+        for (int j = i; j < 100; j += i) {
+            lijun[j] = !lijun[j];
+        }
+    }
+    
+    for (int i = 0; i < 100; i++) {
+        cout << lijun[i] << "\t";
+    }
+    
+    return 0;
 }
