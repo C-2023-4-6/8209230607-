5c173f4ce0f0593c4129264293cc96795a6f3ca1
ljzaizheli <150006378+ljzaizheli@users.noreply.github.com>
2023-12-12 12:53:25 +0800

    Update 实验四11.cpp

--- cpp/实验四11.cpp
+++ cpp/实验四11.cpp
@@ -1,29 +1,39 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int indexof(const char* s1, const char* s2) {
-	int b=0;
-	int len1 = strlen(s1);
-	int len2 = strlen(s2);
-    for (int i = 0; i <= (len2 - len1); i++) { 
-        for (int j = 0; j < len1; j++) {
+    int index = -1; 
+    int s1Len = strlen(s1);
+    int s2Len = strlen(s2);
+    for (int i = 0; i <= s2Len - s1Len; i++) {
+        bool found = true;
+        for (int j = 0; j < s1Len; j++) {
             if (s1[j] != s2[i + j]) {
-                b = -1;
+                found = false;
                 break;
             }
-            b = i;  
         }
-        if (b != -1) {  
-            return b;
+        if (found) {
+            index = i;
+            break;
         }
     }
-    return -1;
+
+    return index;
 }
 int main() {
-	char s1[100] = {};
-	cin >> s1;
-	char s2[100] = {};
-	cin >> s2;
-	cout<<indexof(s1, s2);
+    char s1[100];
+    char s2[100];
+    cout << "请输入字符串s1：";
+    cin.getline(s1, 100);
+    cout << "请输入字符串s2：";
+    cin.getline(s2, 100);
+    int result = indexof(s1, s2);
+    if (result == -1) {
+        cout << "s1不是s2的子串" <<"-1" << endl;
+    }
+    else {
+        cout << "s1是s2的子串，第一次匹配的下标为：" << result << endl;
+    }
 
-	return 0;
+    return 0;
 }
